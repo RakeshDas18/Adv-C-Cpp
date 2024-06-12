6081730c8cb9f29440f1092eb9a0c197cd48b3c8
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-06-12 07:10:44 +0530

    Update Call by value.c

--- Call by value.c
+++ Call by value.c
@@ -2,13 +2,17 @@
 
 #include<stdio.h>
 
+void swap(int a, int b){
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
 int main(){
 	int x = 10;
 	int y = 20;
-	int temp;
-	temp = x;
-	x = y;
-	y = temp;
+	swap(x, y);
 	printf("In call by values: \nX = %d\nY = %d", x, y);
 	
 	return 0;
